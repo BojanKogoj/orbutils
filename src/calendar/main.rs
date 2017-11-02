f760c55d5e02dcf89c7ac137f65dd198af3bdd13
Bojan <bojan.kogoj@gmail.com>
2017-11-02 22:01:13 +0100

    Removed unused crate

--- src/calendar/main.rs
+++ src/calendar/main.rs
@@ -1,5 +1,4 @@
 extern crate orbtk;
-extern crate calc;
 extern crate chrono;
 
 use orbtk::{Rect, Window, Grid, Label, TextBox};
@@ -16,46 +15,44 @@ fn main(){
 
     {
 
-        {
-            let grid = Grid::new();
-            grid.position(8, 8)
-                .spacing(8, 8);
+        let grid = Grid::new();
+        grid.position(8, 8)
+            .spacing(8, 8);
 
-            // TODO(Bojan): Replace with chrono::Weekday
-            // TODO(Bojan): Add start with Sunday option
-            let day_names = &["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        // TODO(Bojan): Replace with chrono::Weekday
+        // TODO(Bojan): Add start with Sunday option
+        let day_names = &["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-            for (i, day) in day_names.iter().enumerate() {
-                let label = Label::new();
-                label.size(cell_width, cell_day_name_height).text(*day);
-                grid.insert(i, 0, &label);
-            }
-
-            let date: DateTime<Local> = Local::now();
-
-            let mut day = 1;
-            for idx in 0..5*7usize {
-
-                if (idx as u32) < date.with_day(1).unwrap().weekday().number_from_monday() -1 {
-                    continue;
-                }
-
-                let d = date.with_day(day);
-                match d {
-                    Some(x) => {
-                        let cell = TextBox::new();
-                        let text = format!("{}", x.day());
-                        let text_offset = cell_width / 2 - (text.len() as u32 * 4);
-                        cell.size(cell_width, cell_height).text(text).text_offset(text_offset as i32, (cell_width / 2 -8) as i32);
-                        grid.insert(idx % 7, (idx / 7) + 1, &cell);
-                    },
-                    None => {}
-                }
-                day += 1;
-            }
-
-            window.add(&grid);
+        for (i, day) in day_names.iter().enumerate() {
+            let label = Label::new();
+            label.size(cell_width, cell_day_name_height).text(*day);
+            grid.insert(i, 0, &label);
         }
+
+        let date: DateTime<Local> = Local::now();
+
+        let mut day = 1;
+        for idx in 0..5*7usize {
+
+            if (idx as u32) < date.with_day(1).unwrap().weekday().number_from_monday() -1 {
+                continue;
+            }
+
+            let d = date.with_day(day);
+            match d {
+                Some(x) => {
+                    let cell = TextBox::new();
+                    let text = format!("{}", x.day());
+                    let text_offset = cell_width / 2 - (text.len() as u32 * 4);
+                    cell.size(cell_width, cell_height).text(text).text_offset(text_offset as i32, (cell_width / 2 -8) as i32);
+                    grid.insert(idx % 7, (idx / 7) + 1, &cell);
+                },
+                None => {}
+            }
+            day += 1;
+        }
+
+        window.add(&grid);
     }
 
     window.exec();
